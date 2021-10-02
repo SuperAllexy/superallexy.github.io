e027f233deb5f8e2dacc898e7f58a0e4d7c6f17c
SuperAllexy <53560064+SuperAllexy@users.noreply.github.com>
2021-10-02 22:28:40 +0300

    Update scrypt.js

--- js/scrypt.js
+++ js/scrypt.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 jQuery(function($){
 	$(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $(".menu_item"); // тут указываем ID элемента
+		var div = $(".hamburger"); // тут указываем ID элемента
 		if (!div.is(e.target) // если клик был не по нашему блоку
 		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
 			div.hide(); // скрываем его
