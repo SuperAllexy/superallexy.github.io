ebf8a700fa2d88c713f20b12d1807deb39712dc3
SuperAllexy <53560064+SuperAllexy@users.noreply.github.com>
2021-10-02 22:30:42 +0300

    Update scrypt.js

--- js/scrypt.js
+++ js/scrypt.js
@@ -15,12 +15,3 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
-jQuery(function($){
-	$(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $("hamburger_active"); // тут указываем ID элемента
-		if (!div.is(e.target) // если клик был не по нашему блоку
-		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
-			div.hide(); // скрываем его
-		}
-	});
-});
